59851dfe0e2a97cd02c5f003380bbf8ee984fe4d
enlore <n.e.lorenson@gmail.com>
2013-11-09 17:54:23 -0600

    Localized config

--- app.js
+++ app.js
@@ -1,28 +1,40 @@
 var express = require('express')
     , app = express()
     , port = 9022
+    , ip = ''
     , less = require('less-middleware')
     , path = require('path')
 
 app.set('view engine', 'jade')
 app.set('views', __dirname + '/views')
 
+// development
 app.configure('development', function () {
     app.locals.pretty = true
+    ip = 'localhost'
+})
+
+// production
+app.configure('production', function () {
 })
 
 app.use(less({
     src: path.join(__dirname, 'static'),
     compress: false,
-    debug: true,
+    debug: false,
     paths: [path.join(__dirname, 'static')]
 }))
 
 app.use(express.static(path.join(__dirname, 'static')))
+app.use(express.logger())
 
 app.get('/', function (req, res) {
     res.render('index')
 })
 
-app.listen(port)
+app.get('/menus', function(req, res) {
+    res.render('menus')
+})
+
+app.listen(port, ip)
 console.log('* listening on ' + port)
