24e266fa93f2cb0ee1f57f27f5b436588fc71116
enlore <n.e.lorenson@gmail.com>
2013-11-10 16:32:51 -0600

    Google maps js

--- static/js/main.js
+++ static/js/main.js
@@ -34,3 +34,15 @@ $(function () {
 
     Page.init()
 });
+
+function initialize() {
+    var mapOptions = {
+        center      : new google.maps.LatLng(36.5274667, -87.3596),
+        // 36.5274667, 87.3596
+        zoom        : 8,
+        mapTypeId   : google.maps.MapTypeId.ROADMAP
+    }
+    var map = new google.maps.Map(document.getElementById("map-canvas"),
+            mapOptions)
+}
+google.maps.event.addDomListener(window, 'load', initialize)
