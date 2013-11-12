2ddac0d9bc2e71f81b45aac6672dedbb341de4a3
enlore <n.e.lorenson@gmail.com>
2013-11-12 16:22:13 -0600

    Set deployment address

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var express = require('express')
     , ip = ''
     , less = require('less-middleware')
     , path = require('path')
+    , less_opts = {}
 
 app.set('view engine', 'jade')
 app.set('views', __dirname + '/views')
@@ -12,18 +13,21 @@ app.set('views', __dirname + '/views')
 app.configure('development', function () {
     app.locals.pretty = true
     ip = '192.168.1.141'
+    less_opts.src: path.join(__dirname, 'static'),
+    less_opts.compress: false,
+    less_opts.debug: true,
+    less_opts.paths: [path.join(__dirname, 'static')]
 })
 
 // production
 app.configure('production', function () {
+    app.locals.pretty = false
+    ip = '127.0.0.1'
+    less_opts.compress = true
+    less_opts.debug = false
 })
 
-app.use(less({
-    src: path.join(__dirname, 'static'),
-    compress: false,
-    debug: false,
-    paths: [path.join(__dirname, 'static')]
-}))
+app.use(less(less_opts))
 
 app.use(express.static(path.join(__dirname, 'static')))
 app.use(express.logger())
